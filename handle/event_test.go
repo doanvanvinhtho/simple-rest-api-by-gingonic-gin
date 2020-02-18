4b0214b632aa2ac94c5bd31edd34323817a27cce
Tho Doan <6262201+doanvanvinhtho@users.noreply.github.com>
2020-02-18 22:20:35 +0700

    Add some tests

--- handle/event_test.go
+++ handle/event_test.go
@@ -1,6 +1,7 @@
 package handle
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -12,8 +13,12 @@ import (
 )
 
 type testcase struct {
-	inID     string
-	wantCode int
+	inID            string
+	inTitle         string
+	inDescription   string
+	wantCode        int
+	wantTitle       string
+	wantDescription string
 }
 
 func TestGetOneEvent(t *testing.T) {
@@ -22,12 +27,20 @@ func TestGetOneEvent(t *testing.T) {
 
 	cases := []testcase{
 		{
-			inID:     "id_go",
-			wantCode: 200,
+			inID:            "id_go",
+			inTitle:         "Go",
+			inDescription:   "https://golang.org/",
+			wantCode:        200,
+			wantTitle:       "Go",
+			wantDescription: "https://golang.org/",
 		},
 		{
-			inID:     "12345_54321",
-			wantCode: 404,
+			inID:            "12345_54321",
+			inTitle:         "",
+			inDescription:   "",
+			wantCode:        404,
+			wantTitle:       "",
+			wantDescription: "",
 		},
 	}
 
@@ -44,7 +57,194 @@ func TestGetOneEvent(t *testing.T) {
 		err := json.Unmarshal([]byte(w.Body.String()), &response)
 
 		assert.Nil(t, err)
+		assert.Equal(t, c.wantCode, response.Code)
 
+		if response.Code == 200 {
+			resData := response.Data.(map[string]interface{})
+			assert.Equal(t, c.wantTitle, resData["title"])
+			assert.Equal(t, c.wantDescription, resData["description"])
+		}
+	}
+}
+
+func TestGetAllEvent(t *testing.T) {
+	Init("inmemory")
+	router := Setup()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/events", nil)
+	router.ServeHTTP(w, req)
+
+	// The http response code always 200
+	assert.Equal(t, 200, w.Code)
+
+	// Convert the JSON response to object
+	var response model.Response
+	err := json.Unmarshal([]byte(w.Body.String()), &response)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, 200, response.Code)
+}
+
+func TestAddEvent(t *testing.T) {
+	Init("inmemory")
+	router := Setup()
+
+	cases := []testcase{
+		{
+			inID:            "id_new_1",
+			inTitle:         "title_new_1",
+			inDescription:   "description_new_1",
+			wantCode:        200,
+			wantTitle:       "title_new_1",
+			wantDescription: "description_new_1",
+		},
+	}
+
+	for _, c := range cases {
+		w := httptest.NewRecorder()
+
+		reqEventJSON, _ := json.Marshal(model.Event{
+			ID:          c.inID,
+			Title:       c.inTitle,
+			Description: c.inDescription,
+		})
+		req, _ := http.NewRequest("POST", "/events", bytes.NewBuffer([]byte(reqEventJSON)))
+		req.Header.Set("Content-Type", "application/json")
+
+		router.ServeHTTP(w, req)
+
+		// The http response code always 200
+		assert.Equal(t, 200, w.Code)
+
+		// Convert the JSON response to object
+		var response model.Response
+		err := json.Unmarshal([]byte(w.Body.String()), &response)
+
+		assert.Nil(t, err)
+		assert.Equal(t, c.wantCode, response.Code)
+
+		// Get the event back to check
+		w = httptest.NewRecorder()
+
+		req, _ = http.NewRequest("GET", "/events/"+c.inID, nil)
+		router.ServeHTTP(w, req)
+
+		// The http response code always 200
+		assert.Equal(t, 200, w.Code)
+
+		// Convert the JSON response to object
+		err = json.Unmarshal([]byte(w.Body.String()), &response)
+
+		assert.Nil(t, err)
+		assert.Equal(t, c.wantCode, response.Code)
+
+		if response.Code == 200 {
+			resData := response.Data.(map[string]interface{})
+			assert.Equal(t, c.wantTitle, resData["title"])
+			assert.Equal(t, c.wantDescription, resData["description"])
+		}
+	}
+}
+
+func TestUpdateEvent(t *testing.T) {
+	Init("inmemory")
+	router := Setup()
+
+	cases := []testcase{
+		{
+			inID:            "id_go",
+			inTitle:         "title_new_1",
+			inDescription:   "description_new_1",
+			wantCode:        200,
+			wantTitle:       "title_new_1",
+			wantDescription: "description_new_1",
+		},
+	}
+
+	for _, c := range cases {
+		w := httptest.NewRecorder()
+
+		reqEventJSON, _ := json.Marshal(model.Event{
+			ID:          c.inID,
+			Title:       c.inTitle,
+			Description: c.inDescription,
+		})
+		req, _ := http.NewRequest("PATCH", "/events", bytes.NewBuffer([]byte(reqEventJSON)))
+		req.Header.Set("Content-Type", "application/json")
+
+		router.ServeHTTP(w, req)
+
+		// The http response code always 200
+		assert.Equal(t, 200, w.Code)
+
+		// Convert the JSON response to object
+		var response model.Response
+		err := json.Unmarshal([]byte(w.Body.String()), &response)
+
+		assert.Nil(t, err)
+		assert.Equal(t, c.wantCode, response.Code)
+
+		// Get back the event
+		w = httptest.NewRecorder()
+
+		req, _ = http.NewRequest("GET", "/events/"+c.inID, nil)
+		router.ServeHTTP(w, req)
+
+		// The http response code always 200
+		assert.Equal(t, 200, w.Code)
+
+		// Convert the JSON response to object
+		err = json.Unmarshal([]byte(w.Body.String()), &response)
+
+		assert.Nil(t, err)
+		assert.Equal(t, c.wantCode, response.Code)
+
+		if response.Code == 200 {
+			resData := response.Data.(map[string]interface{})
+			assert.Equal(t, c.wantTitle, resData["title"])
+			assert.Equal(t, c.wantDescription, resData["description"])
+		}
+	}
+}
+
+func TestDeleteEvent(t *testing.T) {
+	Init("inmemory")
+	router := Setup()
+
+	cases := []testcase{
+		{
+			inID:            "id_go",
+			inTitle:         "",
+			inDescription:   "",
+			wantCode:        404,
+			wantTitle:       "",
+			wantDescription: "",
+		},
+	}
+
+	for _, c := range cases {
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("DELETE", "/events/"+c.inID, nil)
+		router.ServeHTTP(w, req)
+
+		// The http response code always 200
+		assert.Equal(t, 200, w.Code)
+
+		// Get the event to make sure it doesn't exist
+		w = httptest.NewRecorder()
+		req, _ = http.NewRequest("GET", "/events/"+c.inID, nil)
+		router.ServeHTTP(w, req)
+
+		// The http response code always 200
+		assert.Equal(t, 200, w.Code)
+
+		// Convert the JSON response to object
+		var response model.Response
+		err := json.Unmarshal([]byte(w.Body.String()), &response)
+
+		assert.Nil(t, err)
 		assert.Equal(t, c.wantCode, response.Code)
 	}
 }
