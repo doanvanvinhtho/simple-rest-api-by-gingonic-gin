b7d435074e42a47d5cb38f1a9d7634c10011d77d
Tho Doan <6262201+doanvanvinhtho@users.noreply.github.com>
2020-02-19 15:31:29 +0700

    Redis CRUD

--- repository/redis/README.md
+++ repository/redis/README.md
@@ -3,6 +3,6 @@
 ``` bash
 docker container exec -it simple-rest-api-by-gingonic-gin_redis_1 redis-cli
 
-HMSET event:id_go ID id_go Title "Go" Description "https://golang.org/ (Redis)"
-HMSET event:id_gin ID id_gin Title "Gin" Description "https://github.com/gin-gonic/gin (Redis)"
+HSET event:id_go ID id_go Title "Go" Description "https://golang.org/ (Redis)"
+HSET event:id_gin ID id_gin Title "Gin" Description "https://github.com/gin-gonic/gin (Redis)"
 ```
--- repository/redis/event_redis.go
+++ repository/redis/event_redis.go
@@ -29,7 +29,6 @@ func (e *eventRedis) GetOneEvent(id string) (*model.Event, error) {
 	// HGETALL: returns all fields and values of the hash stored at key.
 	// In the returned value, every field name is followed by its value,
 	// so the length of the reply is twice the size of the hash.
-
 	values, err := redis.Values(conn.Do("HGETALL", "event:"+id))
 	if err != nil {
 		return nil, err
@@ -48,7 +47,44 @@ func (e *eventRedis) GetOneEvent(id string) (*model.Event, error) {
 
 // GetAllEvent helps to get all events from repository
 func (e *eventRedis) GetAllEvent() ([]model.Event, error) {
-	return nil, nil
+	var resultEvent []model.Event
+
+	conn := e.pool.Get()
+	defer conn.Close()
+
+	scanResult, err := redis.MultiBulk(conn.Do("SCAN", 0, "MATCH", "event:*", "COUNT", 20))
+	if err != nil {
+		return nil, err
+	}
+
+	// iterator, err := redis.Int(scanResult[0], nil)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	keys, err := redis.Strings(scanResult[1], nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, k := range keys {
+		values, err := redis.Values(conn.Do("HGETALL", k))
+		if err != nil {
+			return nil, err
+		} else if len(values) <= 0 {
+			return nil, errors.New("[Redis] Event " + k + " not found")
+		}
+
+		var elmEvent model.Event
+		err = redis.ScanStruct(values, &elmEvent)
+		if err != nil {
+			return nil, err
+		}
+
+		resultEvent = append(resultEvent, elmEvent)
+	}
+
+	return resultEvent, nil
 }
 
 // AddEvent helps to add new event into repository
@@ -56,7 +92,8 @@ func (e *eventRedis) AddEvent(ev *model.Event) (string, error) {
 	if ev == nil {
 		return "", errors.New("[Redis] The event need to add is nil")
 	}
-	return "", nil
+
+	return e.UpdateEvent(ev)
 }
 
 // UpdateEvent helps to update an event in repository
@@ -64,7 +101,23 @@ func (e *eventRedis) UpdateEvent(ev *model.Event) (string, error) {
 	if ev == nil {
 		return "", errors.New("[Redis] The event need to update is nil")
 	}
-	return "", nil
+
+	conn := e.pool.Get()
+	defer conn.Close()
+
+	// HSET: Sets field in the hash stored at key to value.
+	// If key does not exist, a new key holding a hash is created.
+	// If field already exists in the hash, it is overwritten
+	_, err := conn.Do("HSET", "event:"+ev.ID,
+		"ID", ev.ID,
+		"Title", ev.Title,
+		"Description", ev.Description,
+	)
+	if err != nil {
+		return "", err
+	}
+
+	return ev.ID, nil
 }
 
 // DeleteEvent helps to delete an event in repository
@@ -72,5 +125,15 @@ func (e *eventRedis) DeleteEvent(id string) error {
 	if len(id) <= 0 {
 		return errors.New("[Redis] The event id is empty")
 	}
+
+	conn := e.pool.Get()
+	defer conn.Close()
+
+	// DEL: Removes the specified keys. A key is ignored if it does not exist.
+	_, err := conn.Do("DEL", "event:"+id)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
